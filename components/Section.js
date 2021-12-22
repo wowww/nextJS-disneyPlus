1374f6a5c811d86a6caed8d652e3cf4e2c42a358
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-22 12:55:40 +0900

    docs. 비디오를 해당 slug로 보내줌

--- components/Section.js
+++ components/Section.js
@@ -1,11 +1,21 @@
+import Card from './Card';
 
-
-
-const Section = ({genre}) => {
+const Section = ({ genre, videos }) => {
 
   return (
-    <div>
+    <div className="section">
       <h3>{genre}</h3>
+      <div className="video-feed">
+        {videos && videos.map(video => (
+          <a 
+            key={video.id}
+            href={`/video/${video.slug}`}
+          >
+            <Card thumbnail={video.thumbnail} />
+
+          </a>
+        ))}
+      </div>
     </div>
   )
 }
