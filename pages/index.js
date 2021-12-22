bee16cb24515794141c814318779a6ba807023a1
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-22 10:42:13 +0900

    feat. GraphQL을 활용해 생성한 데이터 가져옴
    - ContentAPI, TokenValue에 각각 해당하는 API, Value 값  삽입

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,43 @@
-const Home = () => {
+import { gql, GraphQLClient } from 'graphql-request'
+// import gql from 'graphql-tag'
+
+export const getStaticProps = async() => {
+  const url = "ContentAPI";
+  const graphQLClient = new GraphQLClient(url, {
+    headers: {
+      "Authorization": "Bearer TokenValue"
+    }
+  })
+
+  const query = gql`
+    query {
+      videos {
+        createdAt,
+        id,
+        title,
+        description,
+        seen,
+        slug,
+        tags
+      }
+    }
+  `
+
+  const data = await graphQLClient.request(query)
+  const videos = data.videos
+
+  return {
+    props: {
+      videos,
+    }
+  }
+}
+
+
+const Home = ({ videos }) => {
+  console.log(videos)
   return (
     <div>
-      
     </div>
   )
 }
