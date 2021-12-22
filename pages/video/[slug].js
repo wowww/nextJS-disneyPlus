81e8070194ff4b956fb8477a724cf727af44294f
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-22 14:38:57 +0900

    feat. 비디오가 보일 때, 안보일 때 설명 등의 상세내용 처리

--- pages/video/[slug].js
+++ pages/video/[slug].js
@@ -1,4 +1,5 @@
 import { gql, GraphQLClient } from 'graphql-request'
+import { useState } from 'react'
 
 export const getServerSideProps = async (pageContext) => {
   const url = process.env.ENDPOINT
@@ -47,11 +48,43 @@ export const getServerSideProps = async (pageContext) => {
 }
 
 const video = ({ video }) => {
-  console.log(video)
+  const [ watching, setWatching ] = useState(false)
   return (
-    <div>
+    <>
+      { !watching &&
+        <img 
+          className="video-image"
+          src={video.thumbnail.url}
+          alt={video.title}
+        />
+      }
+      { !watching &&
+        <div className="info">
+          <p>{video.tags.join(', ')}</p>
+          <p>{video.description}</p>
+          <a href="/">
+            <span>
+              go back
+            </span>
+          </a>
+          <button 
+            className="video-overlay"
+            onClick={() => {
+              watching ? setWatching(false) : setWatching(true)
+            }} 
+          >PLAY</button>
+        </div>
+      }
+      { watching && (
+        <video width="100%" controls>
+          <source 
+            src={video.mp4.url} 
+            type="video/mp4/" 
+          />
 
-    </div>
+        </video>
+      )}
+    </>
   )
 }
 
