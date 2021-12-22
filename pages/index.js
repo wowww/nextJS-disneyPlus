cc89aa69f7877f061b0bdbcc2b1ab1ef1a76c290
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-22 13:41:08 +0900

    docs. unSeenVideos 추가

--- pages/index.js
+++ pages/index.js
@@ -50,8 +50,13 @@ const Home = ({ videos }) => {
     return videos.filter((video) => video.tags.includes(genre))
   }
 
+  const unSeenVideos = (videos) => {
+    return videos.filter((video) => video.seen === false | video.seen === null)
+  }
+
   return (
     <>
+    
       <div className="app">
         <div className="main-video">
           <img 
@@ -59,17 +64,17 @@ const Home = ({ videos }) => {
             alt={randomVideo(videos).title}
           />
         </div>
-      <div className="video-feed">
-        <Section genre={'Recommeded for you'} />
-        <Section genre={'Family'} videos={filterVideos(videos, 'family')} />
-        <Section genre={'Thriller'} videos={filterVideos(videos, 'thriller')} />
-        <Section genre={'Classic'} videos={filterVideos(videos, 'classic')} />
-        <Section genre={'Pixar'} videos={filterVideos(videos, 'pixar')} />
-        <Section genre={'Marvel'} videos={filterVideos(videos, 'marvel')} />
-        <Section genre={'National Geographic'} videos={filterVideos(videos, 'national-geographic')} />
-        <Section genre={'Disney'} videos={filterVideos(videos, 'disney')} />
-        <Section genre={'Star Wars'} videos={filterVideos(videos, 'star-wars')} />
-      </div>
+        <div className="video-feed">
+          <Section genre={'Recommeded for you'} videos={unSeenVideos(videos)} />
+          <Section genre={'Family'} videos={filterVideos(videos, 'family')} />
+          <Section genre={'Thriller'} videos={filterVideos(videos, 'thriller')} />
+          <Section genre={'Classic'} videos={filterVideos(videos, 'classic')} />
+          <Section genre={'Pixar'} videos={filterVideos(videos, 'pixar')} />
+          <Section genre={'Marvel'} videos={filterVideos(videos, 'marvel')} />
+          <Section genre={'National Geographic'} videos={filterVideos(videos, 'national-geographic')} />
+          <Section genre={'Disney'} videos={filterVideos(videos, 'disney')} />
+          <Section genre={'Star Wars'} videos={filterVideos(videos, 'star-wars')} />
+        </div>
       </div>
 
 
